9cd030c95424bf1715ac6ffbcb797b9c7799a5b6
Jeremie Berduck <jeremieberduck@gmail.com>
2025-06-17 13:49:19 +0200

    Initiating Project

---
+++ Rick & Morty/Main/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rick & Morty
+//
+//  Created by Jeremie Berduck on 17/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
