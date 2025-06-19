8cae7876cb1afced4f4ccff52e72d363d496be28
Jeremie Berduck <jeremieberduck@gmail.com>
2025-06-19 10:52:14 +0200

    Episode detail

--- Rick & Morty/views/EpisodeDetailView.swift
+++ Rick & Morty/views/EpisodeDetailView.swift
@@ -11,14 +11,32 @@ struct EpisodeDetailView: View {
     
     var selectedEpisode: Episode?
     
+    let columns = Array(repeating: GridItem(.fixed(64)), count: 5)
+    
+    @State private var selectedCharacterID: Int?
+    
     var body: some View {
         if let selectedEpisode {
             ScrollView {
-                VStack(alignment: .leading) {
-                    EpisodeNumber(number: selectedEpisode.episode)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding()
+                Text("Characters ID")
+                    .font(.title2).fontWeight(.semibold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                LazyVGrid(columns: columns, spacing: 16) {
+                    ForEach(selectedEpisode.characterIDs, id: \.self) { characterID in
+                        Button(action: {
+                            self.selectedCharacterID = characterID
+                        }){
+                            ZStack {
+                                Circle()
+                                    .fill(Color.orange.opacity(0.6))
+                                    .frame(width: 64, height: 64)
+                                Text("\(characterID)")
+                                    .font(.headline).fontWeight(.bold).fontDesign(.rounded)
+                            }
+                        }.accentColor(Color.white)
+                    }
+                }.padding()
             }
             .navigationTitle(selectedEpisode.name)
         } else {
